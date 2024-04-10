99c4209bae6cf43fec108f04b3948372c0c55f7b
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-04-10 13:34:59 +0000

    task 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,8 @@ class DBStorage:
         session_factory = sessionmaker(
             bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(session_factory)()
+    
+    def close(self):
+        """Call remove() method on the private session attribute (`self.__session`)."""
+        if self.__session:
+            self.__session.close()
